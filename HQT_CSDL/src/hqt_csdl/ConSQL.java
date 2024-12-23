3d57d220ff21d6c7cacd46c6a2b79d9c12deb3d5
Iwant2dye8976 <xuanbinh.bx204@gmail.com>
2024-12-23 22:23:01 +0700

    New branch

--- HQT_CSDL/src/hqt_csdl/ConSQL.java
+++ HQT_CSDL/src/hqt_csdl/ConSQL.java
@@ -12,15 +12,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.crypto.spec.PSource;
+import javax.swing.JOptionPane;
 
 
 public class ConSQL implements INhaSach{
     static Connection cn;
+    private ResultSet rs;
     
     public void getCon() {
         try {
             if (cn == null || cn.isClosed()) {
-                cn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=QlyNhaSach;user=sa;password=123456;encrypt=true;trustServerCertificate=true");
+                cn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=QlyNhaSach;user=sa;password=123456789;encrypt=true;trustServerCertificate=true");
                 System.out.println("ket noi thanh cong");
             }
         } catch (SQLException e) {
@@ -28,23 +30,69 @@ public class ConSQL implements INhaSach{
         }
     }
     
-    public ArrayList<Book> LoadData(){
+    public ResultSet GetBooks(){
         getCon();
+        rs = null;
         String querry = "Select * from Books";
-        ArrayList<Book> ListEmployees = new ArrayList<>();
-        try (PreparedStatement ps = cn.prepareStatement(querry)){
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Book book = new Book(rs.getInt("BookID"), rs.getString("Title"), rs.getString("Author"), 
-                        rs.getString("Category"), rs.getString("Publisher"), rs.getDouble("Price"), 
-                        rs.getInt("StockQuantity"));
-                ListEmployees.add(book);
-            }
-            return ListEmployees;
-        } catch (Exception e) {
+        try{
+            PreparedStatement ps = cn.prepareStatement(querry);
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return rs;
     }
     
+    public void AddBook(String Title, String Author, String Category, String Publisher, double Price, int StockQuantity){
+        String sql = "Insert into Books(Title, Author, Category, Publisher, Price, StockQuantity) Values(?,?,?,?,?,?)";
+        getCon();
+        try{
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1, Title);
+            ps.setString(2, Author);
+            ps.setString(3, Category);
+            ps.setString(4, Publisher);
+            ps.setDouble(5, Price);
+            ps.setInt(6, StockQuantity);
+            ps.execute();
+            JOptionPane.showMessageDialog(null, "Thêm thành công!", null, JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void UpdateBook(int BookID, String Title, String Author, String Category, String Publisher, double Price, int StockQuantity){
+        String sql = "Update Books set Title=?, Author=?, Category=?, Publisher=?, Price=?, StockQuantity=? Where BookID=?";
+        getCon();
+        try{
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1, Title);
+            ps.setString(2, Author);
+            ps.setString(3, Category);
+            ps.setString(4, Publisher);
+            ps.setDouble(5, Price);
+            ps.setInt(6, StockQuantity);
+            ps.setInt(7, BookID);
+            ps.execute();
+            JOptionPane.showMessageDialog(null, "Cập nhật thành công!", null, JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+        
+    public void DeleteBook(int BookID) {
+        String sql = "Delete Books Where BookID=?";
+        getCon();
+        try{
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setInt(1, BookID);
+            ps.execute();
+            JOptionPane.showMessageDialog(null, "Xóa thành công!", null, JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
